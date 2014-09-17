bd37ce323dc8a5c686138a4ff322682985fe4d70
root <root@localhost.localdomain>
2014-09-17 17:54:00 +0200

    add script getKernelSource.sh

---
+++ rock_pro/scripts/getKernelSource.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+##########################################################################################################
+# Paths and variables
+##########################################################################################################
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+
+basedir=../
+kerneldir=${basedir}/kernel_rockchip
+
+
+##########################################################################################################
+# Functions
+##########################################################################################################
+
+function getKernel {
+	cd ${basedir}
+	git clone -b radxa/stable-3.0 https://github.com/frep/kernel_rockchip.git
+	cd ${kerneldir}
+	echo "Patching kernel"
+	wget http://patches.aircrack-ng.org/mac80211.compat08082009.wl_frag+ack_v1.patch -O mac80211.patch
+	patch -p1 --no-backup-if-mismatch < mac80211.patch
+	touch .scmversion
+}
+
+
+##########################################################################################################
+# program
+##########################################################################################################
+
+#check, if kernel-directory already exists
+if [ -d ${kerneldir} ]; then
+	while true; do
+		read -p "Kernel directory already exists. Delete and [r]eexport or [s]kip ?" rs
+		case $rs in
+		[Rr]* )	rm -rf ${kerneldir};
+			getKernel;
+			exit;;
+		[Ss]* )	exit;;
+		* )	echo "Please answer [r] or [s].";;
+		esac
+	done
+fi
+
+getKernel
+
+exit
