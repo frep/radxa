59cbc44faf096390c9ee6e69b66c4c63a6421a43
frep <frehnerp@gmail.com>
2014-09-20 17:49:59 +0200

    more tools added

--- rock_pro/scripts/getTools.sh
+++ rock_pro/scripts/getTools.sh
@@ -44,6 +44,28 @@ function installRkflashtool {
 	cd ..
 }
 
+function getPackTools {
+	git clone https://github.com/frep/rockchip-pack-tools
+}
+
+function getUpgradeTool {
+	mkdir upgradeTool
+	cd upgradeTool
+	wget http://dl.radxa.com/rock/tools/linux/Linux_Upgrade_Tool_v1.16.zip
+	unzip Linux_Upgrade_Tool_v1.16.zip
+	cd ..
+}
+
+function getRkflashkit {
+	git clone https://github.com/linuxerwang/rkflashkit
+	cd rkflashkit
+	./waf debian
+	apt-get install python-gtk2
+	dpkg -i rkflashkit_0.1.2_all.deb
+	cd ..
+}
+
+
 ##########################################################################################################
 # program
 ##########################################################################################################
@@ -122,29 +144,52 @@ else
         installRkflashtool
 fi
 
+# get rockchip-pack-tools
+if [ -d rockchip-pack-tools ]; then
+        while true; do
+                read -p "Pack-tools directory already exists. Delete and [r]eimport or [s]kip ?" rs
+                case $rs in
+                [Rr]* ) rm -rf rockchip-pack-tools;
+                        getPackTools;
+                        break;;
+                [Ss]* ) break;;
+                * )     echo "Please answer [r] or [s].";;
+                esac
+        done
+else
+        getPackTools
+fi
 
-############################################################################
-# Upgrade tool to flash parameter-linux
-############################################################################
-#mkdir upgradeTool
-#cd upgradeTool
-#wget http://dl.radxa.com/rock/tools/linux/Linux_Upgrade_Tool_v1.16.zip
-#unzip Linux_Upgrade_Tool_v1.16.zip
-#cp upgrade_tool ../
-#cd ${tooldir}
+# get upgrade tool to flash parameter-linux
+if [ -d upgradeTool ]; then
+        while true; do
+                read -p "upgrade-tool directory already exists. Delete and [r]eimport or [s]kip ?" rs
+                case $rs in
+                [Rr]* ) rm -rf upgradeTool;
+                        getUpgradeTool;
+                        break;;
+                [Ss]* ) break;;
+                * )     echo "Please answer [r] or [s].";;
+                esac
+        done
+else
+        getUpgradeTool
+fi
 
-############################################################################
-# GUI-based tool to flash and backup partitions
-############################################################################
-#git clone https://github.com/linuxerwang/rkflashkit 
-#cd rkflashkit
-#./waf debian
-#apt-get install python-gtk2
-#dpkg -i rkflashkit_0.1.2_all.deb
-
-############################################################################
-# rockchip-pack-tools: Tools to create an update.img
-############################################################################
-#git clone https://github.com/radxa/rockchip-pack-tools.git
+# get rkflashkit (GUI-based tool to flash and backup partitions)
+if [ -d rkflashkit ]; then
+        while true; do
+                read -p "rkflashkit directory already exists. Delete and [r]eimport or [s]kip ?" rs
+                case $rs in
+                [Rr]* ) rm -rf rkflashkit;
+                        getRkflashkit;
+                        break;;
+                [Ss]* ) break;;
+                * )     echo "Please answer [r] or [s].";;
+                esac
+        done
+else
+        getRkflashkit
+fi
 
 exit
