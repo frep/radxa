bf3d8dee3a1b0c0d5b967805064c5683f9fc8cff
root <frehnerp@gmail.com>
2014-09-30 00:16:05 +0200

    more post-installation

--- rock_pro/files/post-installation.sh
+++ rock_pro/files/post-installation.sh
@@ -35,3 +35,23 @@
 #dpkg -i chromium/chromium-browser_37.0.2062.120-0ubuntu0.12.04.1~pkg917_armhf.deb chromium/chromium-codecs-ffmpeg-extra_37.0.2062.120-0ubuntu0.12.04.1~pkg917_armhf.deb
 #tar xvfz chromium/PepperFlash-12.0.0.77-armv7h.tar.gz -C /usr/lib
 #cp -f chromium/default /etc/chromium-browser/default
+
+#install java
+#mkdir -p -v /opt/java
+#tar xvzf jdk-8u6-linux-arm-vfp-hflt.tar.gz -C /opt/java
+#update-alternatives --install "/usr/bin/java" "java" "/opt/java/jdk1.8.0_06/bin/java" 1
+#update-alternatives --set java /opt/java/jdk1.8.0_06/bin/java
+#echo "JAVA_HOME=\"/opt/java/jdk1.8.0_06\"" >> /etc/enviroment
+#echo "" >> /root/.bashrc
+#echo "export JAVA_HOME=\"/opt/java/jdk1.8.0\"" >> /root/.bashrc
+#echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /root/.bashrc
+
+#install arduino
+#apt-get install arduino -y
+#mkdir -p /lib/modules/3.0.36+/kernel/drivers/usb/class
+#cp cdc-acm.ko /lib/modules/3.0.36+/kernel/drivers/usb/class/
+#depmod -a
+#echo "cdc-acm" >> /etc/modules
+
+#fix wireshark error
+#cp -f init.lua /usr/share/wireshark/init.lua
