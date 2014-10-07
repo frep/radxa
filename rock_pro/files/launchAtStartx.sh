be359fab1509bb553901c7032dc8c59faf205330
frep <frehnerp@gmail.com>
2014-10-07 07:44:21 +0200

    Some comments added

--- rock_pro/files/launchAtStartx.sh
+++ rock_pro/files/launchAtStartx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# if a conky instance is already running, kill it first
 killall conky
-
+# give system time to get ready, before starting conky
 sleep 10
-
+# start conky as background task
 conky &
 
 exit
